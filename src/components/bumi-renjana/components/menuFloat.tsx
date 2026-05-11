fa8b0a70bf14b51126f604460385b9cb77283b2e
fadilkun45 <indocrafter76@gmail.com>
2026-05-11 12:39:04 +0700

    feat: menu float

--- src/components/bumi-renjana/components/menuFloat.tsx
+++ src/components/bumi-renjana/components/menuFloat.tsx
@@ -28,7 +28,7 @@ const MenuFloat = () => {
 
   return (
     // Wrapper utama, di-set fixed di pojok kanan bawah
-    <div className="fixed bottom-10 right-6 flex flex-col items-center gap-3 z-30">
+    <div className="fixed bottom-10 cursor-pointer right-6 flex flex-col items-center gap-3 z-30">
       
       {/* Container untuk menu shortcut */}
       <div 

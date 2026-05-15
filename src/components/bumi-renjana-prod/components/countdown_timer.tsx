e77b3762a17daccec465ee2f6e100c53903de122
Faldi Ramadhan <indocrafter76@gmail.com>
2026-05-16 03:12:10 +0700

    fix lagi

--- src/components/bumi-renjana-prod/components/countdown_timer.tsx
+++ src/components/bumi-renjana-prod/components/countdown_timer.tsx
@@ -5,22 +5,31 @@ interface CountdownTimerProps {
   targetDate: string | Date;
 }
 
+
+
 export default function CountdownTimer({ targetDate }: CountdownTimerProps) {
-  const calculateTimeLeft = () => {
-    const difference = +new Date(targetDate) - +new Date();
-    let timeLeft = { days: 0, hours: 0, minutes: 0, seconds: 0 };
+ const calculateTimeLeft = () => {
+  const target = new Date(
+    new Date(targetDate).toLocaleString("en-US", { timeZone: "Asia/Jakarta" })
+  );
+  const now = new Date(
+    new Date().toLocaleString("en-US", { timeZone: "Asia/Jakarta" })
+  );
+  const difference = +target - +now;
+  
+  let timeLeft = { days: 0, hours: 0, minutes: 0, seconds: 0 };
 
-    if (difference > 0) {
-      timeLeft = {
-        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-        minutes: Math.floor((difference / (1000 * 60)) % 60),
-        seconds: Math.floor((difference / 1000) % 60),
-      };
-    }
+  if (difference > 0) {
+    timeLeft = {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / (1000 * 60)) % 60),
+      seconds: Math.floor((difference / 1000) % 60),
+    };
+  }
 
-    return timeLeft;
-  };
+  return timeLeft;
+};
 
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
 
